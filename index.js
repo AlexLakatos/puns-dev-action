dc0ea1e44db351a31a858941b362372437bff8f8
Alex Lakatos <alex.lakatos.qa@gmail.com>
2021-10-01 21:26:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const puns = require('puns.dev');
 
 try {
   let pun = puns.random();
-  core.setOutput("pun", pun);
-  console.log(`The pun: ${pun}`);
+  core.setOutput("pun", JSON.stringify(pun));
+  console.log(`The pun: ${JSON.stringify(pun)}`);
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
