61168436c8b878a518c025ad7992f7ed88b15200
Belema Gancarz <b.gancarz@cmcmarkets.com>
2023-10-26 13:08:24 +0100

    Upgrade to node 16

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const puns = require('puns.dev');
 
 try {
   let pun = puns.random();
   core.setOutput("pun", pun.pun);
   core.setOutput("punchline", pun.punchline);
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
 } catch (error) {
   core.setFailed(error.message);
 }
